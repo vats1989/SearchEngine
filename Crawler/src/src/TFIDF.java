2a40448a2e16a0761a059bd3f831fedde9928872
Vatsal Sevak <vatsalsevak.2010@gmail.com>
2016-03-12 02:33:03 -0800

    Update TFIDF.java
    Changes to TF-IDF

--- Crawler/src/src/TFIDF.java
+++ Crawler/src/src/TFIDF.java
@@ -65,12 +65,11 @@ public class TFIDF {
 		IndexReader ir = IndexReader.open(dir);
 		//System.out.println("1...."+ir.docFreq(new Term("contents", "vatsal")));
 
-		// Total Number Of Documents
 		int N = ir.maxDoc();
-		
+		String urlPath = "";
+		String firstPath = "";
 		System.out.println("TF Calculation Starts....");
 		for (int docNum = 0; docNum < ir.numDocs(); docNum++) {
-			// System.out.println(ir.document(docNum).getField("path").stringValue());
 			TermFreqVector tfv = ir.getTermFreqVector(docNum, "contents");
 			if (tfv == null || tfv.size() == 0) {
 				continue;
@@ -80,17 +79,17 @@ public class TFIDF {
 			int freqs[] = tfv.getTermFrequencies();
 			//String docName[] = ir.document(docNum).getField("path").stringValue().toString().split("\\\\");
 			//String doc = docName[docName.length - 1];
-			String doc = ir.document(docNum).getField("path").stringValue().toString();
+			urlPath = ir.document(docNum).getField("path").stringValue().toString();
 			// ir.document(docNum).getField("path").stringValue().substring(ir.document(docNum).getField("path").stringValue().lastIndexOf("\\")+1);
 			String tmpurl = "";		
-			String data1[] = doc.split("articles");
+			String data1[] = urlPath.split("articles");
 			if(data1.length > 1)	{
+				firstPath = data1[0];
 				tmpurl = data1[1];
 				tmpurl = tmpurl.replaceAll("\\\\", "/");
 			}
 			
 			for (int t = 0; t < termCount; t++) {
-				// System.out.println(terms[t] + " " + freqs[t]);
 				if (!tfMap.containsKey("" + terms[t])) {
 					Map<String, Integer> map = new HashMap<String, Integer>();
 					map.put(tmpurl, freqs[t]);
@@ -106,68 +105,34 @@ public class TFIDF {
 		
 		// Calculate idf and then tf-idf of each term per each document
 		System.out.println("TF-IDF Calculation Starts....");
+		
 		Map<String, Integer> tempMap1;
-		//Map<String, Double> tempMap2;
 		JSONArray jsonArray;
 		JSONObject jsonObject;
-		String url = "";
 		for (String key : tfMap.keySet()) {
-			Double idf = (double) Math.log10((double) N / tfMap.get(key).size());
-			tempMap1 = (HashMap<String, Integer>) tfMap.get(key);
-			//tempMap2 = new HashMap<String, Double>();
-			jsonArray = new JSONArray();
-			//System.out.println(key);
-			if(key.contains("."))
-				key = key.replaceAll(".", "");
+
+			if(!key.isEmpty() && key.length() != 0 && !key.trim().equals("") )		{
 			
-			for (String str : tempMap1.keySet())	{	
-				String data[] = str.split("articles");
-				if(data.length > 1)	{
-					url = data[1];
-					url = url.replaceAll("\\\\", "/");
-					//System.out.println(" URL : "+url);
+				Double idf = (double) Math.log10((double) N / tfMap.get(key).size());
+				tempMap1 = (HashMap<String, Integer>) tfMap.get(key);
+				jsonArray = new JSONArray();
+				if(key.contains("."))
+					key = key.replaceAll(".", "");
+				
+				for (String str : tempMap1.keySet())	{	
+					System.out.println(" URL : "+str);
+						jsonObject = new JSONObject();
+						jsonObject.put("path", firstPath+"articles"+str.replaceAll("/", "\\\\"));
+						jsonObject.put("tfidf", tempMap1.get(str) * idf);
+						jsonObject.put("url",str);
+						jsonArray.add(jsonObject);
 				}
-				//System.out.println(str);
-				//if(!key.contains("."))	{
-					jsonObject = new JSONObject();
-					jsonObject.put("path", str);
-					jsonObject.put("tfidf", tempMap1.get(str) * idf);
-					jsonObject.put("url",url);
-					jsonArray.add(jsonObject);
-				//}
-				//tempMap2.put(str, tempMap1.get(str) * idf);
+				db.insertScore(key, jsonArray);
 			}
-			db.insertScore(key, jsonArray);
-			//tfidfMap.put(key, (HashMap<String, Double>) tempMap2);
 		}
 		System.out.println("TF-IDF Calculation Finished....");
 		System.out.println("Writing TF-IDF to Database Done....");
-		/*
-		FileOutputStream fos = new FileOutputStream("F:\\CrawlerFiles\\tf_idf_score.txt");
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(tfidfMap);
-		oos.close();
-		System.out.println("Writing Done....");
-		*/
 	}
-
-	/*
-	public Map<String, HashMap<String, Double>> getTFIDFValueMap() {
-		
-		FileInputStream fos;
-		ObjectInputStream oos = null;
-		try {
-			fos = new FileInputStream("F:\\CrawlerFiles\\tf_idf_score.txt");
-			oos = new ObjectInputStream(fos);
-			return (Map<String, HashMap<String, Double>>) oos.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-		
-		return db.getTFIDFFromDB("java");
-	}
-	*/
 	
 	public Map<String, Double> getTFIDFValueByTerm(String term) {
 		return db.getTFIDFFromDB(term);
