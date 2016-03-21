d943edd46e6503468bfefcc648a0b811ccc1f915
Vatsal Sevak <vatsalsevak.2010@gmail.com>
2016-03-21 14:37:37 -0700

    Final Project

--- Crawler/src/src/CS454LinkAnalysis.java
+++ Crawler/src/src/CS454LinkAnalysis.java
@@ -1,152 +1,124 @@
 package src;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import java.io.*;
+import java.util.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML.Attribute;
+import javax.swing.text.html.HTML.Tag;
+import javax.swing.text.html.HTMLEditorKit.ParserCallback;
+import javax.swing.text.html.parser.ParserDelegator;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class CS454LinkAnalysis {
 
 	List<DocumentModel> allDocs = new ArrayList<DocumentModel>();
 	final String indexPath = "F:\\MyIndexing\\indexing";
 	Map<String, HashSet<String>> outLinks = new HashMap<String, HashSet<String>>();
-	Map<String, HashSet<String>> inLinks  = new HashMap<String, HashSet<String>>();
+	Map<String, HashSet<String>> inLinks = new HashMap<String, HashSet<String>>();
 	int documentNumber;
+	Database transactionDB = new Database();
 
-	
 	public static void main(String[] args) throws IOException, ParseException {
 		new CS454LinkAnalysis();
 	}
-	
+
 	public CS454LinkAnalysis() throws IOException {
+		Date start = new Date();
 		getOutGoingLinks();
 		getInComingLinks();
+		Date end = new Date();
+		
+		System.out.println(end.getTime() - start.getTime() + " total milliseconds");
 	}
 
 	@SuppressWarnings("deprecation")
 	private void getOutGoingLinks() throws IOException {
-		
+
 		Directory dir = FSDirectory.open(new File(indexPath));
 		IndexReader ir = IndexReader.open(dir);
-		File storageFile;
+		FileReader storageFile;
 		Set<String> set;
 		DocumentModel dm;
 		int totalDoc = ir.maxDoc();
 		setDocumentNumber(ir.maxDoc());
-		
+
 		for (int docNum = 0; docNum < ir.numDocs(); docNum++) {
-			String doc = ir.document(docNum).getField("path").stringValue().toString();
-			//String docName[] = ir.document(docNum).getField("path").stringValue().toString().split("\\\\");
-			//String url = ir.document(docNum).getField("url").stringValue().toString();
-			
-			String data[] = doc.split("articles");
-			if(data.length > 1)	{
-				String url = data[1];
-				url = url.replaceAll("\\\\", "/");
-				System.out.println(docNum+" URL : "+doc);
-				
-				storageFile = new File(doc);
-				Document document = Jsoup.parse(storageFile, "ISO-8859-1");
-				
-				set = new HashSet<String>();			
-				dm = new DocumentModel();
-				dm.setId(""+docNum+1);
-				dm.setPath(doc);
-				dm.setUrl(url);
-				dm.setDefaultRank((double)1/totalDoc);
-				
-				Elements links = document.select("a");
-				for(Element link : links)	{
-					String t = link.attr("abs:href");
-					
-					if(!t.isEmpty())	{
-						System.out.println("5....."+t);
-						String temp[] = t.split("articles");
-						if(temp.length > 1)	{
-							set.add(temp[1]);
-							System.out.println("1..."+temp[1]);
-						}
-					}
-				}	
-				dm.setOutGoingLink(set);
-				outLinks.put(url, (HashSet<String>) set);
-				allDocs.add(dm);
-				System.out.println("####### "+dm.getOutGoingLink().size());
-			}
+
+			String filePath = ir.document(docNum).getField("path").stringValue().toString();
+			String documentUrl = ir.document(docNum).getField("url").stringValue().toString();
+
+			storageFile = new FileReader(filePath);
+			List<String> links = extractLinks(storageFile);
+
+			set = new HashSet<String>(links);
+			dm = new DocumentModel();
+			dm.setId("" + docNum + 1);
+			dm.setPath(filePath);
+			dm.setUrl(documentUrl);
+			dm.setDefaultRank((double) 1 / totalDoc);
+			dm.setOutGoingLink(set);
+
+			outLinks.put(documentUrl, (HashSet<String>) set);
+			allDocs.add(dm);
+
+			//System.out.println(docNum + " " + documentUrl + "->" + set.size());
 		}
 	}
-	/*
-	 * print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-	public String trim(String s, int width) {
-        if (s.length() > width)
-            return s.substring(0, width-1) + ".";
-        else
-            return s;
-    }
-	*/
 
-	public void getInComingLinks()	{
+	public void getInComingLinks() {
 		Set<String> list;
-		for(DocumentModel dm1 : allDocs)	{
+		for (DocumentModel dm1 : allDocs) {
 			list = new HashSet<String>();
-			for(DocumentModel dm2 : allDocs )	{
-				if(dm1.getUrl() != dm2.getUrl())
-					if(dm2.getOutGoingLink().contains(dm1.getUrl()))
-							list.add(dm2.getUrl());
+			for (DocumentModel dm2 : allDocs) {
+				if (dm1.getUrl() != dm2.getUrl())
+					if (dm2.getOutGoingLink().contains(dm1.getUrl()))
+						list.add(dm2.getUrl());
 			}
 			inLinks.put(dm1.getUrl(), (HashSet<String>) list);
 			dm1.setInComingLink(list);
-			System.out.println(dm1.getUrl()+":"+list.size());
+			//System.out.println(list.size() + ":" + dm1.getUrl());
 		}
 	}
-	
-	public List<DocumentModel> getAllLinks() {
-		return allDocs;
+
+	public List<String> extractLinks(Reader reader) throws IOException {
+		final ArrayList<String> list = new ArrayList<String>();
+		ParserDelegator parserDelegator = new ParserDelegator();
+		ParserCallback parserCallback = new ParserCallback() {
+			public void handleText(final char[] data, final int pos) {}
+
+			public void handleStartTag(Tag tag, MutableAttributeSet attribute, int pos) {
+				if (tag == Tag.A) {
+					String address = (String) attribute
+							.getAttribute(Attribute.HREF);
+					list.add(address);
+				}
+			}
+			public void handleEndTag(Tag t, final int pos) {}
+			public void handleSimpleTag(Tag t, MutableAttributeSet a, final int pos) {}
+			public void handleComment(final char[] data, final int pos) {}
+			public void handleError(final java.lang.String errMsg, final int pos) {}
+		};
+		parserDelegator.parse(reader, parserCallback, true);
+		return list;
 	}
 
-	public void setAllLinks(List<DocumentModel> allLinks) {
-		this.allDocs = allLinks;
+	public List<DocumentModel> getAllLinks() {
+		return allDocs;
 	}
 
 	public Map<String, HashSet<String>> getOutLinks() {
 		return outLinks;
 	}
 
-	public void setOutLinks(Map<String, HashSet<String>> outLinks) {
-		this.outLinks = outLinks;
-	}
-
 	public Map<String, HashSet<String>> getInLinks() {
 		return inLinks;
 	}
 
-	public void setInLinks(Map<String, HashSet<String>> inLinks) {
-		this.inLinks = inLinks;
-	}
-
-	public String getIndexPath() {
-		return indexPath;
-	}
-
 	public List<DocumentModel> getAllDocs() {
 		return allDocs;
-	}
-
-	public void setAllDocs(List<DocumentModel> allDocs) {
-		this.allDocs = allDocs;
 	}
 
 	public int getDocumentNumber() {
@@ -155,5 +127,5 @@ public class CS454LinkAnalysis {
 
 	public void setDocumentNumber(int documentNumber) {
 		this.documentNumber = documentNumber;
-	}	
+	}
 }
